3f48d07b4073b3eee8dddf8d8cb5057603a083ab
janvandijk96 <janvandijk1996@gmail.com>
2015-05-28 13:45:20 +0200

    PHP Fix

--- php/menu.php
+++ php/menu.php
@@ -1,6 +1,6 @@
     <nav class="menu">
       <img src="img/logo.png" alt="logo" class="menulogo"/>
-      <h1 class="menulogotext"><?php echo $naam; ?></h1>
+      <h1 class="menulogotext">Five Elephants</h1>
       <ul>
         <li><a class="menulink" href="">Home</a></li>
         <li><a class="menulink" href="">About Us</a></li>

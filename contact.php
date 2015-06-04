202e7ade4fc1c31566034d4dab0ee42ce3f8d833
hobiewetzels <hobie.wetzels@gmail.com>
2015-06-04 10:59:07 +0200

    contact pagina vervangen met de code van index
    herp derp

--- contact.php
+++ contact.php
@@ -3,63 +3,28 @@ session_start();
 ?>
 <html>
   <head>
-    <meta charset="utf-8" />
-    <title>Five Elephants</title>
-   
-    <!--Import CSS Styles -->
-    <link href="css/reset.css" rel="stylesheet" /> <!-- Reset All CSS Styles -->
-    <link href="css/animate.css" rel="stylesheet" /> <!-- Animations Stylesheet by Internet -->
-    <link href="css/style.css" rel="stylesheet" /> <!-- Default Stylesheet -->
-    <link href="fonts/fonts.css" rel="stylesheet" /> <!-- Import Custom Fonts -->
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" /> <!-- Import Font Awesome (For icons and shit) -->
-
-    <!-- Import Script Files -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/script.js"  type="text/javascript"></script>
-
-    <!-- Send Google Analytics Stats -->
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-61967638-3', 'auto');
-      ga('send', 'pageview');
-    </script>
-
-
+    <?php include('php/head.php'); ?>
   </head>
   <body>
 
   <div class="splash">
-  <img src="img/logo.png" id="splashlogo">
-  <h1 id="splashtekst">Five Elephants</h1>
+    <img src="img/logo.png" id="splashlogo">
+    <h1 id="splashtekst">Five Elephants</h1>
   </div>
 
-    <div class="menu">
-      <img src="img/logo.png" alt="logo" class="menulogo"/>
-      <h1 class="menulogotext"><?php echo $naam; ?></h1>
-      <ul>
-        <li><a class="menulink" href="">Home</a></li>
-        <li><a class="menulink" href="">About Us</a></li>
-        <li><a class="menulink" href="">Hallo</a></li>
-        <li><a class="menulink" href="">Work</a></li>
-        <li><a class="menulink" href="">Contact</a></li>
-      </ul>
-    </div>
+    <?php include('php/menu.php'); ?>
     <div class="container">
       <div class="content">
 
     
        
-       <iframe width='900px' height='400px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/janvandijk96.ma52f1n0/attribution,zoompan,zoomwheel,geocoder.html?access_token=pk.eyJ1IjoiamFudmFuZGlqazk2IiwiYSI6IjBUemhpM3cifQ.2HfIkJg7fpJ2_1a0UJHuFw'></iframe>
+       
 
-        
-        <h1>Cup of Coffee?</h1>
+        <img src="http://placehold.it/921x400" />
+        <h1>About Us</h1>
         
         <div id="test"><p>
-        Lorem ipsum dolor sit amet, vix et suas mediocritatem, cibo error in eam, has ne clita decore postea. Has falli oportere efficiendi et. Id vis eius referrentur. In ius summo viris imperdiet, ei nec vocibus eligendi, mandamus gubergren rationibus eu sea. No dico laudem est, vel ea porro eirmod veritus.
+        Hobie Lorem ipsum dolor sit amet, vix et suas mediocritatem, cibo error in eam, has ne clita decore postea. Has falli oportere efficiendi et. Id vis eius referrentur. In ius summo viris imperdiet, ei nec vocibus eligendi, mandamus gubergren rationibus eu sea. No dico laudem est, vel ea porro eirmod veritus.
 
 Per movet percipit cu, nec at erat mucius. Essent principes deseruisse eum et. Apeirian explicari sed ea, at sit iudico postulant patrioque. In sit omnes tollit tibique, id essent maluisset molestiae pro, nec nominati posidonium concludaturque te. Natum mutat soluta et pro, eu illud ignota volumus duo. Bonorum reprehendunt nec eu, blandit moderatius ad duo, his an vocent vidisse elaboraret.
 
@@ -68,13 +33,9 @@ Ne mea summo delicata, sed molestiae vulputate eu. Malis tamquam prompta eam et,
      
      
      </p></div>
-     <h2>Contact Form</h2>
-
      
      
-     
+     <?php include('php/footer.php'); ?>
       </div>
-    <div class="content">
-    </div>
   </body>
 </html>

f87c21f353e27dd8a22e6d8c7984013d2fd4f11d
janvandijk96 <janvandijk1996@gmail.com>
2015-06-04 10:58:27 +0200

    Marco is mijn bitch

---
+++ php/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "ohm-media.nl";
+$username = "janvaye117_fivee";
+$password = "banaan";
+$database = "janvaye117_fivee";
+
+// Create connection
+$con = new mysqli($servername, $username, $password, $database);
+?>

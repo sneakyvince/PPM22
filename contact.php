8c572e1b40673cb1ae62fd3803aa08a1be9c5e2f
Evilkevin22 <kevingrond@gmail.com>
2015-06-11 12:09:42 +0200

    sneaky Jan > Sneaky vince

--- contact.php
+++ contact.php
@@ -1,30 +1,33 @@
-<?php
-session_start();
-?>
-  <head>
-    <?php include('php/head.php'); ?>
-  </head>
-  <body>
+<?php session_start(); ?>
 
-  <div class="splash">
-    <img src="img/logo.png" id="splashlogo">
-    <h1 id="splashtekst">Five Elephants</h1>
-  </div>
+<head>
+    <?php include( 'php/head.php'); ?>
+</head>
 
-    <?php include('php/menu.php'); ?>
+<body>
+
+    <div class="splash">
+        <img src="img/logo.png" id="splashlogo">
+        <h1 id="splashtekst">Five Elephants</h1>
+    </div>
+
+    <?php include( 'php/menu.php'); ?>
     <div class="container">
-      <div class="content">
-        <img src="http://placehold.it/921x400" />
-        <h1>About Us</h1>
-        
-     <form action="" method="post">
-<h1>Bericht</h1>
-<input type="text" name="bericht" id="bericht">
-<h1>Naam</h1>
-<input type="text" name="naam" id="naam">
-<h1>E-mail</h1>
-<input type="text" name="email" id="email">
-<div><input type="button" value="verstuur" id="checkall" onclick = "checkAll()"></td></div>
-  	</form>
-</body> 
-     <?php include('php/footer.php'); ?>
+        <div class="content">
+            <img src="http://placehold.it/921x400" />
+            <h1>About Us</h1>
+
+            <form action="" method="post">
+                <h1>Bericht</h1>
+                <input type="text" name="bericht" id="bericht">
+                <h1>Naam</h1>
+                <input type="text" name="naam" id="naam">
+                <h1>E-mail</h1>
+                <input type="text" name="email" id="email">
+                <div>
+                    <input type="button" value="verstuur" id="checkall" onclick="checkAll()">
+                    </td>
+                </div>
+            </form>
+</body>
+<?php include( 'php/footer.php'); ?>

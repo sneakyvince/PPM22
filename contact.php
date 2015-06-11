487b7a3a9e7a3450584cab1481103535bbc02ff4
hobiewetzels <hobie.wetzels@gmail.com>
2015-06-11 12:17:02 +0200

    gooby pls

--- contact.php
+++ contact.php
@@ -13,26 +13,6 @@
 
     <?php include( 'php/menu.php'); ?>
     <div class="container">
-
-        <div class="content">
-            <img src="http://placehold.it/921x400" />
-            <h1>About Us</h1>
-
-            <form action="" method="post">
-                <h1>Bericht</h1>
-                <input type="text" name="bericht" id="bericht">
-                <h1>Naam</h1>
-                <input type="text" name="naam" id="naam">
-                <h1>E-mail</h1>
-                <input type="text" name="email" id="email">
-                <div>
-                    <input type="button" value="verstuur" id="checkall" onclick="checkAll()">
-                    </td>
-                </div>
-            </form>
-</body>
-<?php include( 'php/footer.php'); ?>
-=======
       <div class="content">
         <img src="http://placehold.it/921x400" />
         <h1>Contact</h1>

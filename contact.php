d475bc7231a10a9521ad353ffcc44987e8cb658b
hobiewetzels <hobie.wetzels@gmail.com>
2015-06-11 11:51:50 +0200

    contact form styling

--- contact.php
+++ contact.php
@@ -17,14 +17,26 @@ session_start();
         <img src="http://placehold.it/921x400" />
         <h1>About Us</h1>
         
-     <form action="" method="post">
-<h1>Bericht</h1>
-<input type="text" name="bericht" id="bericht">
-<h1>Naam</h1>
-<input type="text" name="naam" id="naam">
-<h1>E-mail</h1>
-<input type="text" name="email" id="email">
-<div><input type="button" value="verstuur" id="checkall" onclick = "checkAll()"></td></div>
-  	</form>
+<form action="" method="post" class="basic-grey">
+      <label>
+        <span>Bericht</span>
+        <textarea id="bericht" name="bericht" placeholder="Your Message to Us"></textarea>
+    </label> 
+    
+    <label>
+        <span>Naam</span>
+        <input id="naam" type="text" name="naam" placeholder="Your Full Name" />
+    </label>
+    
+    <label>
+        <span>E-mail</span>
+        <input id="email" type="email" name="email" placeholder="Valid Email Address" />
+    </label>
+    
+     <label>
+        <span>&nbsp;</span> 
+        <input type="button" class="button" value="Verstuur" onclick="checkAll()" /> 
+    </label>    
+</form>
 </body> 
      <?php include('php/footer.php'); ?>

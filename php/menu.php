ebfb77a3d380f7f6d0be2efe26346868f1b31094
janvandijk96 <janvandijk1996@gmail.com>
2015-06-14 23:52:10 +0200

    Andere menu namen

--- php/menu.php
+++ php/menu.php
@@ -3,8 +3,8 @@
       <h1 class="menulogotext">Five Elephants</h1>
       <ul>
         <li><a class="menulink" href="index.php">Home</a></li>
-        <li><a class="menulink" href="about.php">About Us</a></li>
-        <li><a class="menulink" href="projecten.php">Work</a></li>
+        <li><a class="menulink" href="about.php">Over Ons</a></li>
+        <li><a class="menulink" href="projecten.php">Projecten</a></li>
         <li><a class="menulink" href="contact.php">Contact</a></li>
       </ul>
     </nav>
